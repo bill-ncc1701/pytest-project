14ce77018413b55d44261f11c3df03b5da55f096
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-18 09:42:32 -0300

    Refatoração do Logon com Pytest

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -2,15 +2,14 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from conftest import driver
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
+class TestCT01:
+    def test_login_valido(self):
+        # Fazendo o Login
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(2)
+        assert driver.find_element(By.XPATH,"//span[@class='title']").is_displayed()
 
-
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
-time.sleep(2)
-assert driver.find_element(By.XPATH,"//span[@class='title']").is_displayed()
