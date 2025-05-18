effc08bd262bab31bf6810d2c27a1e50d1c9f6e1
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-18 09:17:05 -0300

    Criação do primeiro teste com Pytest

---
+++ tests/test_simulacao_pytest.py
@@ -0,0 +1,16 @@
+import pytest
+
+class TestSimulacao():
+
+    @pytest.mark.simulacao
+    def test_sumulacao_1(self):
+        assert 1 == 1
+
+    @pytest.mark.simulacao
+    @pytest.mark.skip
+    def test_sumulacao_2(self):
+        assert 2 > 1
+
+    @pytest.mark.teste2
+    def test_sumulacao_3(self):
+        assert 20 < 50
