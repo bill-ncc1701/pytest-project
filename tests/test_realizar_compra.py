76cbc00ba015142ae5c77094719de36bc528028b
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-18 09:44:49 -0300

    Refatoração da compra com Pytest

--- tests/test_realizar_compra.py
+++ tests/test_realizar_compra.py
@@ -2,86 +2,84 @@
 import time
 from asyncio import wait_for
 
-from selenium import webdriver
-from selenium.webdriver.common.alert import Alert
+
 from selenium.webdriver.common.by import By
+from conftest import driver
 from selenium.webdriver.support.wait import WebDriverWait
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
+
+class TestCT02:
+    def test_realizar_compra(self):
+        # Realizando uma compra no site
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
 
 
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
+        # Wait for the alert to appear (you might need to adjust the time based on your page load time)
+        time.sleep(5)
 
+        #Alert(driver).accept()
 
-# Wait for the alert to appear (you might need to adjust the time based on your page load time)
-time.sleep(5)
+        # Escolher o produto
+        driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Sauce Labs Backpack']").click()
 
-#Alert(driver).accept()
+        time.sleep(3)
+        # Adicionar o produto ao carrinho
+        driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
+        # Verificar se o produto foi adicionado ao carrinho
+        driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
 
-# Escolher o produto
-driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Sauce Labs Backpack']").click()
+        #Voltando para a lista de produtos
+        driver.find_element(By.XPATH,"//button[@class='btn btn_secondary back btn_medium']").click()
 
-time.sleep(3)
-# Adicionar o produto ao carrinho
-driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
-# Verificar se o produto foi adicionado ao carrinho
-driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+        #Escolhendo a blusa Laranja
 
-#Voltando para a lista de produtos
-driver.find_element(By.XPATH,"//button[@class='btn btn_secondary back btn_medium']").click()
+        # Escolher o produto
+        driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Test.allTheThings() T-Shirt (Red)']").click()
 
-#Escolhendo a blusa Laranja
+        time.sleep(5)
+        # Adicionar o produto ao carrinho
+        driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
+        # Verificar se o produto foi adicionado ao carrinho
+        driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+        #Voltando para a lista de produtos
+        driver.find_element(By.XPATH,"//button[@class='btn btn_secondary back btn_medium']").click()
 
-# Escolher o produto
-driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Test.allTheThings() T-Shirt (Red)']").click()
+        #Escolhendo o macacão para Bebẽ
+        driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Sauce Labs Onesie']").click()
 
-time.sleep(5)
-# Adicionar o produto ao carrinho
-driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
-# Verificar se o produto foi adicionado ao carrinho
-driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
-#Voltando para a lista de produtos
-driver.find_element(By.XPATH,"//button[@class='btn btn_secondary back btn_medium']").click()
+        time.sleep(5)
+        # Adicionar o produto ao carrinho
+        driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
+        # Verificar se o produto foi adicionado ao carrinho
+        driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+        time.sleep(5)
 
-#Escolhendo o macacão para Bebẽ
-driver.find_element(By.XPATH,"//*[@class='inventory_item_name ' and text()='Sauce Labs Onesie']").click()
+        #Conluir a compra (CHECKOUT)
+        driver.find_element(By.XPATH,"//button[@class='btn btn_action btn_medium checkout_button ']").click()
 
-time.sleep(5)
-# Adicionar o produto ao carrinho
-driver.find_element(By.XPATH,"//*[text()='Add to cart']").click()
-# Verificar se o produto foi adicionado ao carrinho
-driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
-time.sleep(5)
+        #Preenchendo os dados do checkout
+        first_name = "Willian"
+        last_name = "Cevassi"
+        postal_code = "86828-000"
 
-#Conluir a compra (CHECKOUT)
-driver.find_element(By.XPATH,"//button[@class='btn btn_action btn_medium checkout_button ']").click()
+        driver.find_element(By.ID,"first-name").send_keys(first_name)
+        driver.find_element(By.ID,"last-name").send_keys(last_name)
+        driver.find_element(By.ID,"postal-code").send_keys(postal_code)
 
-#Preenchendo os dados do checkout
-first_name = "Willian"
-last_name = "Cevassi"
-postal_code = "86828-000"
+        #clicar no botão CONTINUAR
+        driver.find_element(By.ID,"continue").click()
 
-driver.find_element(By.ID,"first-name").send_keys(first_name)
-driver.find_element(By.ID,"last-name").send_keys(last_name)
-driver.find_element(By.ID,"postal-code").send_keys(postal_code)
+        #Testando se caiu na tela final
+        assert driver.find_element(By.XPATH,"//*[@class='header_secondary_container']").is_displayed() , "Não foi possível encontrar a tela de checkout"
 
-#clicar no botão CONTINUAR
-driver.find_element(By.ID,"continue").click()
+        #Finalizando a compra
+        driver.find_element(By.ID, "finish").click()
+        time.sleep(1)
 
-#Testando se caiu na tela final
-assert driver.find_element(By.XPATH,"//*[@class='header_secondary_container']").is_displayed() , "Não foi possível encontrar a tela de checkout"
+        #Testa se caiu na tela de finalização
+        assert driver.find_element(By.XPATH,"//h2[@class='complete-header' ]").is_displayed() , "Não foi possível encontrar a tela de finalização"
 
-#Finalizando a compra
-driver.find_element(By.ID, "finish").click()
-time.sleep(1)
+        time.sleep(2)
 
-#Testa se caiu na tela de finalização
-assert driver.find_element(By.XPATH,"//h2[@class='complete-header' ]").is_displayed() , "Não foi possível encontrar a tela de finalização"
-
-time.sleep(2)
-driver.quit()
