cd74d3ce0fbc6d75528cbcbf770e1634caa78042
bill-ncc1701 <bill_ncc_1701@proton.me>
2025-05-18 09:50:37 -0300

    Criação do teste para login inválido.

---
+++ tests/test_login_invalido.py
@@ -0,0 +1,17 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from conftest import driver
+
+@pytest.mark.usefixtures("setup_teardown")
+class TestCT03:
+    def test_login_invalido(self):
+        # Fazendo o Login com a senha errada.
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("senha-incorreta")
+        driver.find_element(By.ID, "login-button").click()
+        time.sleep(2)
+        assert not driver.find_element(By.XPATH,"//span[@class='title']").is_displayed()
+
